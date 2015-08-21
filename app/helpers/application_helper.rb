9141bd60c3079bee505787399b56c68d4f4591e2
rodrigo <rodrigoabella@gmail.com>
2015-08-21 11:14:08 -0300

    add methods in application helper for declaraciones dataTable

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,43 +2,7 @@ module ApplicationHelper
 
   def norma_a_dispositivos norma
     resp = "dsa"
-    norma.aclaradas.each do |n|
-      resp
-    end
-    norma.delegadas.each do |n|
-      resp
-    end
-    norma.derogadas.each do |n|
-      resp
-    end
     norma.modificadas.each do |n|
-      resp
-    end
-    norma.reglamentadas.each do |n|
-      resp
-    end
-    norma.prorroga_vigencia.each do |n|
-      resp
-    end
-    norma.prorroga_suspension.each do |n|
-      resp
-    end
-    norma.restituye_vigencia.each do |n|
-      resp
-    end
-    norma.suspende_vigencia.each do |n|
-      resp
-    end
-    norma.promulgacion.each do |n|
-      resp
-    end
-    norma.ad_referendum.each do |n|
-      resp
-    end
-    norma.veta_parcialmente.each do |n|
-      resp
-    end
-    norma.veta_totalmente.each do |n|
       resp
     end
     resp  
