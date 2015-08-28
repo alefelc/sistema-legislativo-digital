80764a9a05285201cfe61f63097ee8b5f634909c
rodrigo <rodrigoabella@gmail.com>
2015-08-28 09:26:02 -0300

    change declaracions view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,5 +5,6 @@
 //= require fastclick.min
 //= require dataTables/jquery.dataTables
 //= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
+//= require jquery.tokeninput.min
 //= require_tree .
 //= require turbolinks
