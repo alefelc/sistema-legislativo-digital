0e5e1f63bd110207bba512d842304204f7910da4
rodrigo <rodrigoabella@gmail.com>
2015-09-02 11:22:40 -0300

    add logic tokenInput norma_refer

--- app/controllers/declaracions_controller.rb
+++ app/controllers/declaracions_controller.rb
@@ -9,22 +9,36 @@ class DeclaracionsController < ApplicationController
   end
 
   def search_exp
-      expedientes = Expediente.where("CONCAT(EXTRACT(year from anio), nro_exp, bis) ilike ?",
-                                     "%#{params[:q]}%").first(10)
-      render json: build_json(expedientes)
+    expedientes = Expediente.where("CONCAT(EXTRACT(year from anio), nro_exp, bis) ilike ?",
+                                   "%#{params[:q]}%").first(10)
+    render json: build_json_exp(expedientes)
   end
 
   def search_norma
-      render json: [{name: "roli"}]
+    normas = Norma.where("CONCAT(EXTRACT(year from sancion), nro, bis) ilike ?",
+                                   "%#{params[:q]}%").first(10)
+    render json: build_json_norma(normas)
   end
 
   private
-  def build_json exps
+  
+  def build_json_exp exps
     json_array = []
     exps.each do |x|
       json_array << {
         id: x.id,
         indice: x.anio.year.to_s + "/" + x.nro_exp + "/" + x.bis.to_s
+      }
+    end
+    json_array
+  end
+
+  def build_json_norma norma
+    json_array = []
+    norma.each do |x|
+      json_array << {
+        id: x.id,
+        indice: x.type + ": " + x.sancion.year.to_s + "/" + x.nro.to_s + "/" + x.bis.to_s
       }
     end
     json_array
