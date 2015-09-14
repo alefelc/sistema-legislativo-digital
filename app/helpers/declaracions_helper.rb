72fa29afd7c52758850782f05aec9bae71175139
rodrigo <rodrigoabella@gmail.com>
2015-09-14 11:05:38 -0300

    fix method calsification norm

--- app/helpers/declaracions_helper.rb
+++ app/helpers/declaracions_helper.rb
@@ -8,6 +8,6 @@ module DeclaracionsHelper
   end
 
   def search_value_classify(id_clasif, id_norm)
-    Declaracion.find(id_norm).clasificacions.where(nombre: id_clasif).present?
+    Declaracion.find(id_norm).clasificacions.where(nombre: id_clasif).present? unless id_norm.nil?
   end  
 end
