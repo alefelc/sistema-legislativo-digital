4055dd1f7f2a748ba457336ad54284a0794b1884
Jonathan Leonel Gasparrini <jlgasparrini@gmail.com>
2015-09-21 11:27:40 -0300

    Try new settings for capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,9 +12,10 @@ set :use_sudo, false
 
 set :bundle_binstubs, nil
 
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 
 set :linked_dirs, fetch(:linked_dirs, []).push('shared/log', 'shared/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
 after 'deploy:publishing', 'deploy:restart'
 
 set(:config_files, %w(
