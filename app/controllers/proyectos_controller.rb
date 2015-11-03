549e8482f74fdeedcd592771b9b073e61bb8f205
rodrigo <rodrigoabella@gmail.com>
2015-11-03 09:57:40 -0300

    duplicate view controller proyecto

--- app/controllers/proyectos_controller.rb
+++ app/controllers/proyectos_controller.rb
@@ -4,114 +4,112 @@ class ProyectosController < ApplicationController
   def index
     respond_to do |format|
       format.html
-      format.json { render json: DespachoDatatable.new(view_context) }
+      format.json { render json: ProyectoDatatable.new(view_context) }
     end
   end
 
   def new
-    @despacho = Despacho.new
+    @proyecto = Proyecto.new
     respond_to do |format|
-      format.html {render partial: "modal_despacho", locals: { actionvar: "create"}}
+      format.html {render partial: "modal_proyecto", locals: { actionvar: "create"}}
     end
   end
 
   def show
-    @despacho = Despacho.find(params[:id])
+    @proyecto = Proyecto.find(params[:id])
   end
 
   def edit
-    @despacho = Despacho.find(params[:id])
+    @proyecto = Proyecto.find(params[:id])
     respond_to do |format|
-      format.html {render partial: "modal_despacho", locals: { actionvar: "update"}}
+      format.html {render partial: "modal_proyecto", locals: { actionvar: "update"}}
     end
   end
 
   def destroy
-    Despacho.find(params[:id]).delete
-    render json: {url: "/despachos"}
+    Proyecto.find(params[:id]).delete
+    render json: {url: "/proyectos"}
   end
 
   def create
-    desp = params[:despacho].select { |key, value| ["nro_fojas", "fecha", "observaciones"].include?(key) }
-    @despacho = Despacho.create desp.to_hash
+    pro = params[:proyecto].select { |key, value| ["nro_fojas", "asunto", "updated_at", "observaciones"].include?(key) }
+    @proyecto = Proyecto.create pro.to_hash
 
-    ## get params comisions the POST
-    comisions = params[:despacho][:comisions]
-    unless comisions.blank?
-      @despacho.comisions << Comision.where(id: comisions)
-    end
-
-    ## get params concejals the POST
-    concejals = params[:despacho][:concejals]
-    unless concejals.blank?
-      @despacho.concejals << Concejal.where(id: concejals)
-    end
-
-    ## get params exps_ids the POST
-    expedientes = params[:exps_ids]
-    unless expedientes.blank?
-      @despacho.expedientes << Expediente.where(id: expedientes)
+    ## get params iniciadores the POST
+    unless params[:iniciadores].blank?
+      JSON.parse(params['iniciadores']).each do |key, value|
+        @proyecto.personas << Persona.where(id: value["id"]) if ((value["type"] == "Fisica") || (value["type"] == "Juridica") || (value["type"] == "Concejal"))
+        @proyecto.bloques << Bloque.where(id: value["id"]) if ((value["type"] == "Bloque"))
+        @proyecto.comisions << Comision.where(id: value["id"]) if ((value["type"] == "Comision"))
+      end
     end
 
     redirect_to action: :index
   end
 
   def update
-    desp = params[:despacho].select { |key, value| ["nro_fojas", "fecha","observaciones"].include?(key) }
-    @despacho = Despacho.find(params[:id])
+    pro = params[:proyecto].select { |key, value| ["nro_fojas", "updated_at", "asunto","observaciones"].include?(key) }
+    @proyecto = Proyecto.find(params[:id])
+    @proyecto.update pro.to_hash
 
+    if params['iniciadores'].present?
+      ## update params iniciadores the PATCH
+      current_iniciadores_personas = []
+      current_iniciadores_bloques = []
+      current_iniciadores_comisions = []
+      old_iniciadores_personas = @proyecto.personas.map{ |x| x.id }
+      JSON.parse(params['iniciadores']).each do |key, value|
+        unless old_iniciadores_personas.include?(value["id"])
+          @proyecto.personas << Persona.where(id: value["id"]) if ((value["type"] == "Fisica") || (value["type"] == "Juridica") || (value["type"] == "Concejal"))
+        end
+        current_iniciadores_personas << value["id"]
+      end
 
-    @despacho.update desp.to_hash
+      old_iniciadores_bloques = @proyecto.bloques.map{ |x| x.id }
+      JSON.parse(params['iniciadores']).each do |key, value|
+        unless old_iniciadores_bloques.include?(value["id"])
+          @proyecto.bloques << Bloque.where(id: value["id"]) if ((value["type"] == "Bloque"))
+        end
+        current_iniciadores_bloques << value["id"]
+      end
 
-    ## update params comisions the PATCH
-    current_comisions = params[:despacho][:comisions]
-    old_comisions = @despacho.comisions.map{ |x| x.id.to_s}
-    associated_comisions = (current_comisions - old_comisions)
-    @despacho.comisions << Comision.where(id: associated_comisions)
-    clear_comisions = (old_comisions - current_comisions)
-    @despacho.comisions.delete(Comision.where(id: clear_comisions))
+      old_iniciadores_comisions = @proyecto.comisions.map{ |x| x.id }
+      JSON.parse(params['iniciadores']).each do |key, value|
+        unless old_iniciadores_comisions.include?(value["id"])
+          @proyecto.comisions << Comision.where(id: value["id"]) if ((value["type"] == "Comision"))
+        end
+        current_iniciadores_comisions << value["id"]
+      end
 
-    ## update params concejals the PATCH
-    current_concejals = params[:despacho][:concejals]
-    old_concejals = @despacho.concejals.map{ |x| x.id.to_s}
-    associated_concejals = (current_concejals - old_concejals)
-    @despacho.concejals << Concejal.where(id: associated_concejals)
-    clear_concejals = (old_concejals - current_concejals)
-    @despacho.concejals.delete(Concejal.where(id: clear_concejals))
-
-    ## update params exps_ids the PATCH
-    current_exps = params[:exps_ids].split(',')
-    old_exps = @despacho.expedientes.map{ |x| x.id.to_s}
-    associated_expedientes = (current_exps - old_exps)
-    @despacho.expedientes << Expediente.where(id: associated_expedientes)
-    clear_expedientes = (old_exps - current_exps)
-    @despacho.expedientes.delete(Expediente.where(id: clear_expedientes))
+      # delete iniciadores
+      (old_iniciadores_personas - current_iniciadores_personas).each { |id| @proyecto.personas.delete(id) }
+      (old_iniciadores_bloques - current_iniciadores_bloques).each { |id| @proyecto.bloques.delete(id) }
+      (old_iniciadores_comisions - current_iniciadores_comisions).each { |id| @proyecto.comisions.delete(id) }
+    end
 
     redirect_to action: :index
   end
 
-  def search_exp
-    expedientes = Expediente.where("CONCAT(nro_exp, bis, EXTRACT(year from anio)) ilike ?",
-                                   "%#{params[:q]}%").order("nro_exp::int asc").first(15)
-    render json: build_json_exp(expedientes)
-  end
+  def get_iniciador
+    com = Comision.where("denominacion ilike ?",
+                                   "%#{params[:q]}%").first(7)
+    bloques = Bloque.where("denominacion ilike ?",
+                                   "%#{params[:q]}%").first(7)
+    per = Persona.where("CONCAT(apellido, ' ' , nombre, nro_doc) ilike ?",
+                                   "%#{params[:q]}%").order(apellido: :asc).first(7)
+    com = com.as_json(methods: 'type')
+    bloques = bloques.as_json(methods: 'type')
+    per = per.as_json(methods: 'type' )
+    q = com + bloques + per
+    agregar_nuevo = {"id"=>nil, "nombre"=>"", "apellido"=>"Agregar Nuevo", "tipo_doc"=>nil, "nro_doc"=>"", "telefono"=>"", "email"=>"", "domicilio"=>"", "cargo"=>nil, "bloque_id"=>nil, "created_at"=>nil, "updated_at"=>nil, "cuit"=>0, "type"=>""} 
+    iniciadores = q.push(agregar_nuevo);
+    render json: iniciadores
+  end  
 
   private
 
-  def despacho_params
-    params.require(:despacho).permit("nro_fojas", "fecha",  "observaciones")
-  end
-
-  def build_json_exp exps
-    json_array = []
-    exps.each do |x|
-      year = x.anio.present? ? x.anio.year.to_s : ""
-      json_array << {
-        id: x.id,
-        indice: x.nro_exp + "/" + x.bis.to_s + "/" + year
-      }
-    end
-    json_array
+  def proyecto_params
+    params.require(:proyecto).permit("nro_fojas", "asunto", "updated_at",  "observaciones")
   end
 
 end
---
+++ app/datatables/proyecto_datatable.rb
@@ -0,0 +1,103 @@
+class ProyectoDatatable < AjaxDatatablesRails::Base
+  def_delegator :@view, :index_pro
+  def as_json(options = {})
+    {
+      :draw => params[:draw].to_i,
+      :recordsTotal =>  get_raw_records.count(:all),
+      :recordsFiltered => filter_records(get_raw_records).count(:all),
+      :data => data
+    }
+  end
+
+  def sortable_columns
+    # Declare strings in this format: ModelName.column_name
+    @sortable_columns ||= []
+  end
+
+  def searchable_columns
+    # Declare strings in this format: ModelName.column_name
+    @searchable_columns ||= []
+  end
+
+  private
+
+  def data
+    records.map do |pro|
+      [
+        index_pro(pro),
+        pro.nro_fojas.to_s,
+        get_iniciadores(pro),
+        pro.asunto.to_s,
+        pro.observaciones.to_s,
+        "Estado actual",
+        to_date_time(pro.updated_at),
+        associated_file(pro)
+      ]
+    end
+  end
+
+  def get_iniciadores pro
+    resp = ""
+    iniciadores_bloques = pro.bloques.map{ |x| {type: "Bloque", denominacion: x.denominacion } }
+    iniciadores_bloques.each do |b|
+      resp = resp + b[:type] + ": " + b[:denominacion] + ";\n"
+    end  
+    iniciadores_comisions = pro.comisions.map{ |x| {type: "Comision", denominacion: x.denominacion } }
+    iniciadores_comisions.each do |b|
+      resp = resp + b[:type] + ": " + b[:denominacion] + ";\n"
+    end
+    iniciadores_personas = pro.personas.map{ |x| {type: x.type, apellido: x.apellido, nombre: x.nombre } }
+    iniciadores_personas.each do |b|
+      resp = resp + b[:type] + ": " + b[:apellido] + ", " + b[:nombre] + ";\n"
+    end
+
+    resp
+  end  
+
+  def to_date date
+    date.strftime("%d/%m/%Y") unless date.nil?
+  end
+
+  def to_date_time date
+    date.strftime("%d/%m/%Y - %R") unless date.nil?
+  end  
+
+  def associated_file pro
+    "<div style='display: flex'>" +
+    "<i class='btn btn-xs btn-danger fa fa-times remove-tr' data-remove='#{pro.id}' title='Borrar proyecto'></i>" +
+    "<i class='linktoedit btn btn-xs btn-warning fa fa-pencil-square-o u' data-id='#{pro.id}' title='Editar proyecto'></i>" +
+    "<i class='btn btn-xs btn-success fa fa-download' title='Descargar proyecto'></i></div>"
+  end
+
+  def proyectos
+    fetch_records
+  end
+
+  def fetch_records
+    proyecto = Proyecto.page(page).per(per_page).order(id: :desc)
+    if params[:sSearch].present?
+      unless params[:sSearch].to_i.zero?
+        query = "(tramites.id = #{params[:sSearch]})"
+        proyecto = proyecto.where(query)
+      else
+        query = ""
+        proyecto = proyecto.where(query)
+      end  
+    end
+    proyecto.includes(:bloques).includes(:comisions).includes(:personas)
+  end
+
+  def per_page
+    params[:iDisplayLength].to_i > 0 ? params[:iDisplayLength].to_i : 10
+  end
+
+  def page
+    params[:iDisplayStart].to_i/per_page + 1
+  end
+
+  def get_raw_records
+    Proyecto.all
+  end
+
+  # ==== Insert 'presenter'-like methods below if necessary
+end
