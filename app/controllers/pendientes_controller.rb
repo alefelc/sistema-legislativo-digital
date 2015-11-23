c92e996779121b5847b2fbc6b48bdbbacbce74d5
rodrigo <rodrigoabella@gmail.com>
2015-11-23 08:43:03 -0300

    add controller tramites pendientes

---
+++ app/controllers/pendientes_controller.rb
@@ -0,0 +1,43 @@
+class PendientesController < ApplicationController
+  before_action :authenticate_usuario!
+
+  respond_to :json, :html
+
+  def index
+    respond_to do |format|
+      format.html
+      format.json { render json: PendienteDatatable.new(view_context) }
+    end
+  end
+
+  def new
+    
+  end
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+
+  def destroy
+
+  end
+
+  def create
+    
+  end
+
+  def update
+
+  end
+
+  private
+
+  def pendiente_params
+    params.require(:tramite).permit("nro_fojas", "fecha", "asunto", "observaciones")
+  end
+
+end
