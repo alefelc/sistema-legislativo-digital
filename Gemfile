8af510a9234f59306d4d93181967e915c8c78e1a
Jonathan Leonel Gasparrini <jlgasparrini@gmail.com>
2016-01-12 11:11:48 -0300

    Fix for production deploy

--- Gemfile
+++ Gemfile
@@ -1,13 +1,14 @@
 source 'https://rubygems.org'
 
-ruby '2.2.2'
+#This is required for deploy to heroku
+#ruby '2.2.2'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.1'
 # Use postgresql as the database for Active Record
 gem 'pg'
 # Use MySql legacy database
-gem 'mysql2'
+gem 'mysql2', group: :development
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
@@ -17,7 +18,8 @@ gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
-gem 'rails_12factor'
+# This gem is required for heroku dpeloy
+#gem 'rails_12factor'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
@@ -40,8 +42,10 @@ gem 'rails-i18n'
 # Use Unicorn as the app server
 gem 'unicorn', group: :production
 
-# Use Capistrano for deployment
-gem 'capistrano-rails', group: :development
+# Capistrano for deployment
+gem 'capistrano-rails'
+gem 'capistrano-rvm'
+gem 'capistrano3-unicorn'
 
 # Activeadmin
 gem 'activeadmin', github: 'activeadmin'
---
+++ config/deploy.rb
@@ -0,0 +1,66 @@
+# If the environment differs from the stage name
+set :rails_env, 'production'
+
+# Defaults to 'db'
+set :migration_role, 'migrator'
+
+# Defaults to false
+# Skip migration if files in db/migrate were not modified
+set :conditionally_migrate, true
+
+# Defaults to [:web]
+set :assets_roles, [:web, :app]
+
+# Defaults to 'assets'
+# This should match config.assets.prefix in your rails config/application.rb
+set :assets_prefix, 'prepackaged-assets'
+
+# If you need to touch public/images, public/javascripts, and public/stylesheets on each deploy
+set :normalize_asset_timestamps, %{public/images public/javascripts public/stylesheets}
+
+# Defaults to nil (no asset cleanup is performed)
+# If you use Rails 4+ and you'd like to clean up old assets after each deploy,
+# set this to the number of versions to keep
+set :keep_assets, 2
+
+
+lock '3.4.0'
+
+set :application, 'sld_new'
+set :repo_url, 'git@github.com:jlgasparrini/sistema-legislativo-digital.git'
+
+# Default branch is :master
+set :branch, 'develop'
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+set :use_sudo, false
+
+set :bundle_binstubs, nil
+
+set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+
+set :linked_dirs, fetch(:linked_dirs, []).push('shared/log', 'shared/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+after 'deploy:publishing', 'deploy:restart'
+
+set(:config_files, %w(
+   unicorn.rb
+   unicorn_init.sh
+))
+
+set(:executable_config_files, %w(
+  unicorn_init.sh
+))
+
+set(:symlinks, [
+  {
+  source: "unicorn_new_init.sh",
+  link: "/etc/init.d/unicorn_sld_new"
+  }
+])
+
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
