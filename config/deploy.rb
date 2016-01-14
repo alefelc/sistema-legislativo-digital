4b673b92497540d1b11b0ea412acd7d50dfb3e0d
Jonathan Leonel Gasparrini <jlgasparrini@gmail.com>
2016-01-14 10:07:17 -0300

    Trying new settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,4 @@
-# If the environment differs from the stage name
-set :rails_env, 'production'
-
-# Defaults to 'db'
-set :migration_role, 'migrator'
-
-# Defaults to false
-# Skip migration if files in db/migrate were not modified
-set :conditionally_migrate, true
-
-# Defaults to [:web]
-set :assets_roles, [:app]
-
-# If you need to touch public/images, public/javascripts, and public/stylesheets on each deploy
-set :normalize_asset_timestamps, %{public/images public/javascripts public/stylesheets}
-
-# Defaults to nil (no asset cleanup is performed)
-# If you use Rails 4+ and you'd like to clean up old assets after each deploy,
-# set this to the number of versions to keep
-set :keep_assets, 2
-
-
+# config valid only for current version of Capistrano
 lock '3.4.0'
 
 set :application, 'sld_new'
@@ -41,16 +20,16 @@ after 'deploy:publishing', 'deploy:restart'
 
 set(:config_files, %w(
    unicorn.rb
-   unicorn_init.sh
+   unicorn_init_new.sh
 ))
 
 set(:executable_config_files, %w(
-  unicorn_init.sh
+  unicorn_init_new.sh
 ))
 
 set(:symlinks, [
   {
-  source: "unicorn_new_init.sh",
+  source: "unicorn_init_new.sh",
   link: "/etc/init.d/unicorn_sld_new"
   }
 ])
@@ -60,3 +39,42 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 end
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, '/var/www/my_app_name'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
+# Default value for linked_dirs is []
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+# namespace :deploy do
+
+#   after :restart, :clear_cache do
+#     on roles(:web), in: :groups, limit: 3, wait: 10 do
+#       # Here we can do anything such as:
+#       # within release_path do
+#       #   execute :rake, 'cache:clear'
+#       # end
+#     end
+#   end
+
+# end
