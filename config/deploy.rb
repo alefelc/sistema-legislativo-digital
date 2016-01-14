978053c8d6ea46bcd983b0d9b06c4270402b949c
Jonathan Leonel Gasparrini <jlgasparrini@gmail.com>
2016-01-14 09:37:05 -0300

    Trying to deploy on production

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,7 +37,7 @@ set :use_sudo, false
 
 set :bundle_binstubs, nil
 
-set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 set :linked_dirs, fetch(:linked_dirs, []).push('shared/log', 'shared/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
