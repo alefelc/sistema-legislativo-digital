82904bec1e74e34f27f655b1dfe0e84eca9e09e3
Jonathan Leonel Gasparrini <jlgasparrini@gmail.com>
2016-11-10 09:31:31 -0300

    Add admin bool to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161024132227) do
+ActiveRecord::Schema.define(version: 20161109155608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -693,6 +693,7 @@ ActiveRecord::Schema.define(version: 20161024132227) do
     t.datetime "updated_at",                          null: false
     t.integer  "person_id"
     t.integer  "personal_id"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
