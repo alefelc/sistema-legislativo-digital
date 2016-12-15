b172714d4386ed5f0e26cfb828e65cec79e1b1a5
Jonathan Leonel Gasparrini <jlgasparrini@gmail.com>
2016-12-15 11:56:56 -0300

    Improve admin panel

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,4 +1,5 @@
-ActiveAdmin.register_page "Usuarios" do
+ActiveAdmin.register_page "Registro de usuarios" do
+  menu :parent => "Usuarios"
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
     section "Recently updated content" do
@@ -7,7 +8,7 @@ ActiveAdmin.register_page "Usuarios" do
         # column ("Item") { |v| link_to v.item, [:admin, v.item] } # Uncomment to display as link
         column ("Type") { |v| v.item_type.underscore.humanize }
         column ("Modified at") { |v| v.created_at.to_s :long }
-        column ("Admin") { |v| link_to User.find(v.whodunnit).email, [:admin, User.find(v.whodunnit)] }
+        column ("Admin") { |v| link_to(User.find(v.whodunnit).email, [:admin, User.find(v.whodunnit)]) if v.whodunnit.present?  }
       end
     end
   end
