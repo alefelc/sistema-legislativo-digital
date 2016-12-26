2fb614fee9109f56903f6144e27ab4d06b996549
Jonathan Leonel Gasparrini <jlgasparrini@gmail.com>
2016-12-26 11:16:29 -0300

    Fix drop table migration

--- db/migrate/20161201161337_remove_admin_users.rb
+++ db/migrate/20161201161337_remove_admin_users.rb
@@ -1,5 +1,5 @@
 class RemoveAdminUsers < ActiveRecord::Migration
   def change
-    drop_table :admin_users
+    drop_table :admin_users if ActiveRecord::Base.connection.table_exists? 'admin_users'
   end
 end
