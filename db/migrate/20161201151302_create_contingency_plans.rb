0659de0a5bb88767a74a488fc293d75427f4cd5c
Jonathan Leonel Gasparrini <jlgasparrini@gmail.com>
2017-10-30 11:04:30 -0300

    Fix contingency plans migration and replace logo

--- db/migrate/20161201151302_create_contingency_plans.rb
+++ db/migrate/20161201151302_create_contingency_plans.rb
@@ -1,10 +1,16 @@
 class CreateContingencyPlans < ActiveRecord::Migration
-  def change
-    create_table :contingency_plans do |t|
-      t.text :reason
-      t.date :date_at
+  def up
+    if ActiveRecord::Base.connection.table_exists? 'contingency_plans'
+      create_table :contingency_plans do |t|
+        t.text :reason
+        t.date :date_at
 
-      t.timestamps null: false
+        t.timestamps null: false
+      end
     end
+  end
+
+  def down
+    drop_table :contingency_plans
   end
 end
