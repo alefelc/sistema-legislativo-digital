763d070313613b795b2cfff4fd1a02fd27549279
Jonathan Leonel Gasparrini <jlgasparrini@gmail.com>
2018-09-19 12:22:04 -0300

    Update law types

--- app/helpers/laws_helper.rb
+++ app/helpers/laws_helper.rb
@@ -1,7 +1,6 @@
 module LawsHelper
   def law_types
-    Law.law_types.keys.map do |s|
-      [I18n.t("laws.types.#{s}"), s]
-    end
+    types = [:ordenanza, :declaracion, :resolucion]
+    types.map { |s| [I18n.t("laws.types.#{s}"), s] }
   end
 end
